ea07a32915b27ae09b38fd7f11f9faa57c138d5b
J0J0HA aka. JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2022-06-03 19:02:42 +0200

    Update update.py

--- autodocs/update.py
+++ autodocs/update.py
@@ -7,6 +7,7 @@ import json
 
 def emptydir(dir):
     "Deletes the contents of a directory."
+    print(f"Clearing folder '{dir}'...")
     for file in glob.glob(dir + "/*"):
         try:
             os.remove(file)
@@ -16,22 +17,29 @@ def emptydir(dir):
 
 # Ensure empty docs folder
 try:
+    print("Creating folder '../docs'...")
     os.mkdir("../docs")
 except FileExistsError:
+    print("Folder '../docs' is already existing!")
     emptydir("../docs")
 
 # load config
 with open("config.yml", "r") as file:
+    print("Loading config...")
     config = yaml.safe_load(file)
 
 # configure default data etc.
+print("Constructing extras...")
 extra = ""
 # -> include style
 if "style" in config:
+    print("-> style")
     if "load" in config["style"]:
+        print("Implementing CSS files...")
         for link in config["style"]["load"]:
             extra += '<link rel="stylesheet" href="' + link + '" />'
     if "themes" in config["style"]:
+        print("Implementing themes...")
         extra += '<script src="https://files.jojojux.de/resources/autodocs/themes.js"></script>'
         extra += '<script>themes = ' + json.dumps(config["style"]["themes"]) + ';</script>'
         if "default theme" in config["style"]:
@@ -40,16 +48,19 @@ if "style" in config:
 
 # load template
 with open("template.html", "r") as file:
+    print("Loading template...")
     template = file.read()
 
 # create required folders
 for folder in config["folders"]:
+    print(f"Creating folder '{folder}'...")
     os.mkdir("../docs/" + folder)
 
 # transfer files
 for path in config["files"]:
     for filen in glob.glob("../" + path, recursive=True):
         if filen.endswith(".md"):
+            print(f"Converting '{filen}'...")
             # .md files get converted to html and saved in docs
             with open(filen, "r") as file:
                 raw = file.read()
@@ -63,16 +74,19 @@ for path in config["files"]:
                 )
         else:
             # other files are stored directly in the docs folder
+            print(f"Tranferring '{filen}'...")
             try:
                 with open(filen, "rb") as file:
                     raw = file.read()
                 with open("../docs/" + filen.removeprefix("../"), "wb") as file:
                     file.write(raw)
             except IsADirectoryError:
+                print("-> Is a directory! Skipping...")
                 continue
 
 # creating an optional index page
 if "index" in config:
+    print("Constucting index.html...")
     with open("../" + config["index"], "r") as file:
         raw = file.read()
     html = gh_md_to_html.core_converter.markdown(raw)
