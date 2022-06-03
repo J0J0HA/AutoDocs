3f1a64d8411471debc653b2ed6643b6914cd50c2
J0J0HA aka. JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2022-06-03 16:53:06 +0200

    Create update.py

---
+++ autodocs/update.py
@@ -0,0 +1,28 @@
+import gh_md_to_html.core_converter
+import requests
+import yaml
+import os
+
+try: os.mkdir("docs")
+except FileExistsError: pass
+
+with open("config.yml", "r") as file:
+    config = yaml.safe_load(file)
+
+with open("template.html", "r") as file:
+    template = file.read()
+
+for file in config["files"]:
+    if file.endswith(".md"):
+        raw = requests.get("https://raw.githubusercontent.com/" + config["name"] + "/main/" + file).text
+        html = gh_md_to_html.core_converter.markdown(raw)
+        with open("docs/" + file + ".html", "w") as file:
+            file.write(
+                template
+                    .replace("%title%", str(config["name"]) + "/" + str(file))
+                    .replace("%content%", html)
+            )
+    else:
+        raw = requests.get("https://raw.githubusercontent.com/" + config["name"] + "/main/" + file).text
+        with open("docs/" + file, "w") as file:
+            file.write(raw)
